9cdb08f312c5c14d42fab6e6bb6ee2a98596fd9f
Merhawit Ghebrezadik <ghebremer@gmail.com>
2023-06-06 16:45:59 -0400

    making changes in view for completed repair

--- final_capstone_blank/capstone/vue/src/views/Home.vue
+++ final_capstone_blank/capstone/vue/src/views/Home.vue
@@ -2,11 +2,14 @@
   <div class="home">
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
+    <RequestRepair/>
   </div>
 </template>
-
+<!--calling RequestRepair to display request and provide option to choose complete -->
 <script>
+import RequestRepair from './RequestRepair.vue';
 export default {
+  components: { RequestRepair },
   name: "home"
 };
 </script>
--- final_capstone_blank/capstone/vue/src/views/RequestRepair.vue
+++ final_capstone_blank/capstone/vue/src/views/RequestRepair.vue
@@ -1,10 +1,30 @@
 <template>
   <div>
+    <!-- display repair requests and allow marked as completed -->
+    <h2>Repair Requests</h2>
+    <ul>
+      <li v-for="request in requests" :key="request.id">
+        <div>{{ request.title }}</div>
+        <div>Status: {{ request.status}}</div>
+        <button v-if="request.status !== 'completed'" @click="markCompleted(request.id)">Mark Completed</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {};
+import { mapState, mapMutations } from 'vuex';
+export default {
+  computed: {
+    ...mapState(['requests']),
+  },
+  methods: {
+    ...mapMutations(['markRequestCompleted']),
+    markCompleted(requestId) {
+      this.markRequestCompleted(requestId);
+    }
+  }
+};
 </script>
 
 <style>
