0834435d6818cbd1dd4b09439438fd2a7c62f615
Sarabnidhan Bains <sarabsinghbains@gmail.com>
2023-06-05 22:04:48 -0500

    backend jdbc work

--- final_capstone_blank/capstone/java/database/schema.sql
+++ final_capstone_blank/capstone/java/database/schema.sql
@@ -1,63 +1,30 @@
---BEGIN TRANSACTION;
+BEGIN TRANSACTION;
 
-<<<<<<< HEAD
 
 DROP TABLE IF EXISTS repair_service;
 DROP TABLE IF EXISTS repair_types;
 DROP TABLE IF EXISTS services;
 DROP TABLE IF EXISTS cars;
 DROP TABLE IF EXISTS person;
-=======
-DROP TABLE IF EXISTS repair_status;
-DROP TABLE IF EXISTS repair_costs;
--- DROP TABLE IF EXISTS admins;
->>>>>>> main
 DROP TABLE IF EXISTS users;
--- DROP TABLE IF EXISTS customers;
--- DROP TABLE IF EXISTS employees;
-DROP TABLE IF EXISTS car_info;
-
-CREATE TABLE car_info (
-	car_id SERIAL UNIQUE PRIMARY KEY,
-	color varchar(50),
-	make varchar(50),
-	model varchar(50),
-	year int
-);
 
 
 
 CREATE TABLE users (
-	user_id SERIAL PRIMARY KEY,
+	user_id SERIAL,
 	username varchar(50) NOT NULL UNIQUE,
 	password_hash varchar(200) NOT NULL,
-	role varchar(50) NOT NULL
-	--CONSTRAINT PK_user PRIMARY KEY (user_id)
---	CONSTRAINT fk_group_number FOREIGN KEY (group_number) REFERENCES interest_group(group_number),
-);
---rollback
-
-CREATE TABLE repair_costs (
-	service_id SERIAL PRIMARY KEY,
-	service_type varchar(50) NOT NULL,
-	price int NOT NULL
-	--CONSTRAINT FK_price FOREIGN KEY (price) REFERENCES employees(estimated_price)
+	role varchar(50) NOT NULL,
+	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
---table for repair status
-CREATE TABLE repair_status (
-	car_id int NOT NULL,
-	status varchar(50) NOT NULL,
-	CONSTRAINT FK_car_id PRIMARY KEY (car_id)-- REFERENCES car_info(car_id)
-);
-
-<<<<<<< HEAD
 CREATE TABLE cars (
 	car_id SERIAL,
 	make varchar(50) NOT NULL,
 	model varchar(50) NOT NULL,
 	year int NOT NULL,
 	color varchar(50) NOT NULL,
+	description varchar(200),
 	
 	PRIMARY KEY(car_id)
 );
@@ -91,14 +58,12 @@ CREATE TABLE services(
 
 CREATE TABLE person(
 	user_id int NOT NULL,
-	firstname varchar(200),
-	lastname varchar(200),
-	phone int,
-	email varchar(200),
-	address varchar(200),
-	city varchar(200),
-	state varchar(200),
-	zipcode int,
+	firstname varchar(200) NOT NULL,
+	lastname varchar(200) NOT NULL,
+	username varchar(200 NOT NULL
+	email varchar(200) NOT NULL,
+	phone int NOT NULL,
+	
 	
 	PRIMARY KEY(user_id)
 );
@@ -136,36 +101,3 @@ INSERT INTO repair_types(repair_type, price) VALUES
 
 
 COMMIT TRANSACTION;
-=======
-
--- CREATE TABLE employees (
--- 	user_id int,
--- 	car_id int,
--- 	estimated_price int NOT NULL
---  	--CONSTRAINT PK_car_id PRIMARY KEY (car_id),
--- 	--CONSTRAINT FK_user FOREIGN KEY (user_id) REFERENCES users(user_id)
--- );
-
--- CREATE TABLE customers (
--- 	user_id SERIAL,
--- 	car_id int NOT NULL,
--- 	estimated_price int NOT NULL,
--- 	approved_price boolean NOT NULL,
--- 	--CONSTRAINT FK_user FOREIGN KEY (user_id) REFERENCES users(user_id),
--- 	CONSTRAINT FK_car_id FOREIGN KEY (car_id) REFERENCES employees(car_id)
--- 	--CONSTRAINT FK_price FOREIGN KEY (estimated_price) REFERENCES employees(estimated_price)
--- );
-
--- CREATE TABLE admins (
--- 	user_id SERIAL,
--- 	car_id int NOT NULL,
--- 	--CONSTRAINT FK_user FOREIGN KEY (user_id) REFERENCES users(user_id),
--- 	CONSTRAINT FK_car_id FOREIGN KEY (car_id) REFERENCES employees(car_id)
--- );
-
---table for repair costs, one car to many prices
---rollback
---COMMIT TRANSACTION;
-
---SELECT * FROM customers
->>>>>>> main
---
+++ final_capstone_blank/capstone/java/src/main/java/com/techelevator/model/Car.java
@@ -0,0 +1,70 @@
+package com.techelevator.model;
+
+public class Car {
+
+    private int carId;
+    private String make;
+    private String model;
+    private int year;
+    private String color;
+    private String description;
+
+    public Car(int carId, String make, String model, int year, String color, String description) {
+        this.carId = carId;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.description = description;
+    }
+
+    public Car(){}
+
+    public int getCarId() {
+        return carId;
+    }
+
+    public void setCarId(int carId) {
+        this.carId = carId;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
