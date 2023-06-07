e3e6df15ed30353798be7f3dbb2f7e292720f76d
Jonathan Glenn[D[D[D[D[DParks-Glenn <jongameaddict@aol.com>
2023-06-07 10:36:44 -0400

    filled in the basic starting data for JdbcPriceDao, PriceController, PriceDao, and RepairCost which has now been changed to 'Price'; only basic data, some will need changing like paths and maybe more?

--- final_capstone_blank/capstone/java/src/main/java/com/techelevator/controller/PriceController.java
+++ final_capstone_blank/capstone/java/src/main/java/com/techelevator/controller/PriceController.java
@@ -1,2 +1,36 @@
-package com.techelevator.controller;public class PriceController {
+package com.techelevator.controller;
+//getmapping and postmapping for api work
+//when user selects a service, select an endpoint from here
+//front end can handle the math for prices, then have an endpoint for adding all prices
+
+import com.techelevator.dao.PriceDao;
+import com.techelevator.model.Price;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@PreAuthorize("isAuthenticated()")
+public class PriceController {
+	@Autowired
+	private PriceDao dao;
+	
+	//If our user selects services, we need to read from the database to create service objects, serialize the
+	// service objects into JSON, get the prices of each selected service using this, then add the prices together
+	
+	@GetMapping(path="/allPrices")
+	public List<Price> getAllPrices() {
+		List<Price> allPrices = dao.getPrices();
+		return allPrices;
+	}
+	
+	@GetMapping(path="/price/{id}")
+	public Price returnPriceById(@PathVariable int id) {
+		Price price = dao.getPrice(id);
+		return price;
+	}
+	
 }
--- final_capstone_blank/capstone/java/src/main/java/com/techelevator/dao/JdbcPriceDao.java
+++ final_capstone_blank/capstone/java/src/main/java/com/techelevator/dao/JdbcPriceDao.java
@@ -1,2 +1,50 @@
-package com.techelevator.dao;public class JdbcPriceDao {
+package com.techelevator.dao;
+//talks with database, 'GET * FROM' etc
+
+import com.techelevator.model.Price;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class JdbcPriceDao implements PriceDao {
+	private JdbcTemplate template;
+	
+	public JdbcPriceDao(DataSource dataSource) {
+		template = new JdbcTemplate(dataSource);
+	}
+	
+	@Override
+	public List<Price> getPrices() {
+		String sqlGetAllPrices = "SELECT * FROM prices";
+		List<Price> priceList = new ArrayList<Price>();
+		SqlRowSet rowSet = template.queryForRowSet(sqlGetAllPrices);
+		
+		while (rowSet.next()) {
+			int id = rowSet.getInt("id");
+			String service = rowSet.getString("service");
+			Price price = new Price(id, service);
+			priceList.add(price);
+		}
+		return priceList;
+	}
+	
+	@Override
+	public Price getPrice(int id) {
+		// TODO Auto-generated method stub
+		String getPriceSQL = "SELECT * FROM price where id = ?";
+		SqlRowSet result = template.queryForRowSet(getPriceSQL, id);
+		Price price = null;
+		
+		if (result.next()) {
+			int id2 = result.getInt("id");
+			String service = result.getString("service");
+
+			price = new Price(id2, service);
+		}
+		return price;
+	}
 }
