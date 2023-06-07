521751bc38c8be4770fdcb211169970769545b62
Jonathan Glenn[D[D[D[D[DParks-Glenn <jongameaddict@aol.com>
2023-06-07 18:43:17 -0400

    John Spencer and I started on doing front end math

---
+++ final_capstone_blank/capstone/vue/src/components/PriceComponent.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<div v-for="price in prices" v-bind:key="price.repairId">
+            <div>${{price.price}}</div>
+            <div>{{price.repairId}}</div>
+            <div>{{price.repairType}}</div>
+            <label for="chooseService"></label>
+            <input name="chooseService" type="button" placeholder="Choose a service" />
+            
+        </div>
+	</div>
+</template>
+
+<script>
+import priceService from "../services/PriceService.js";
+
+export default {
+	data() {
+		return {
+			prices: [],
+		};
+	},
+
+	created() {
+		priceService.getAllPrices().then((response) => {
+			if (response.status == 200) {
+				this.prices = response.data;
+			}
+		});
+	},
+};
+</script>
+
+<style>
+</style>
